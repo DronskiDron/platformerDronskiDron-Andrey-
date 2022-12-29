d38fcd7a44a355d1a171e651256590c8fd9ebc17
DronskiDron <andronaf93@gmail.com>
2022-12-29 14:22:58 +0300

    на пути к ДЗ №12

--- Assets/Scripts/Creatures/Weapons/Projectile.cs
+++ Assets/Scripts/Creatures/Weapons/Projectile.cs
@@ -5,6 +5,7 @@ namespace Creatures.Weapons
     public class Projectile : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private bool _invertX;
 
         private Rigidbody2D _rigidbody;
         private int _direction;
@@ -12,8 +13,9 @@ namespace Creatures.Weapons
 
         private void Start()
         {
+            var mod = _invertX ? -1 : 1;
             _rigidbody = GetComponent<Rigidbody2D>();
-            _direction = transform.lossyScale.x > 0 ? 1 : -1;
+            _direction = mod * transform.lossyScale.x > 0 ? 1 : -1;
             Push();
         }
 
