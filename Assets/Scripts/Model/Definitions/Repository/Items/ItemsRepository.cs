4c4d0a42e33e9c16499de5b3775ae326dfdb2f18
DronskiDron <andronaf93@gmail.com>
2023-06-01 17:04:43 +0300

    Домашняя работа №19

--- Assets/Scripts/Model/Definitions/Repository/Items/ItemsRepository.cs
+++ Assets/Scripts/Model/Definitions/Repository/Items/ItemsRepository.cs
@@ -20,10 +20,15 @@ namespace Creatures.Model.Definitions.Repository.Items
     public struct ItemDef : IHaveId
     {
         [SerializeField] private string _id;
+        [SerializeField] private string _itemTitle;
+        [SerializeField] private string _localeKey;
         [SerializeField] private Sprite _icon;
         [SerializeField] private ItemTag[] _tags;
 
         public string Id => _id;
+        public string ItemTitle { get => _itemTitle; set => _itemTitle = value; }
+        public string LocaleKey { get => _localeKey; set => _localeKey = value; }
+
 
         public bool IsVoid => string.IsNullOrEmpty(_id);
         public Sprite Icon => _icon;
---
+++ Assets/Scripts/UI/Localization/LocalizeInventoryText.cs
@@ -0,0 +1,32 @@
+﻿using Creatures.Model.Definitions;
+using Creatures.Model.Definitions.Localisation;
+using Creatures.Model.Definitions.Repository.Items;
+
+namespace UI.Localization
+{
+    public class LocalizeInventoryText : AbstractLocalizeComponent
+    {
+        private ItemDef[] _items;
+
+
+        protected override void Awake()
+        {
+            _items = DefsFacade.I.Items.ItemsForEditor;
+            base.Awake();
+        }
+
+
+        public void OnLocalize()
+        {
+            for (int i = 0; i < _items.Length; i++)
+            {
+                _items[i].ItemTitle = LocalizationManager.I.Localize(_items[i].LocaleKey);
+            }
+        }
+
+        protected override void Localize()
+        {
+
+        }
+    }
+}
