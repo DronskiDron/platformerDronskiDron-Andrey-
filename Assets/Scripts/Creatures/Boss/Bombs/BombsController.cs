f79e49b65a91495c30dc1bfd7b2971cf063442f4
DronskiDron <andronaf93@gmail.com>
2024-01-16 11:07:13 +0300

    Домашняя работа №25

---
+++ Assets/Scripts/Creatures/Boss/Bombs/BombsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using General.Components;
+using UnityEngine;
+
+namespace Creatures.Boss.Bombs
+{
+    public class BombsController : MonoBehaviour
+    {
+        [SerializeField] private BombSequence[] _sequences;
+        private Coroutine _coroutine;
+
+
+        [ContextMenu("Start bombing")]
+        public void StartBombing()
+        {
+            if (_coroutine != null)
+                StopCoroutine(_coroutine);
+
+            _coroutine = StartCoroutine(BombingSequence());
+        }
+
+
+        private IEnumerator BombingSequence()
+        {
+            foreach (var bombSequence in _sequences)
+            {
+
+                foreach (var spawnComponent in bombSequence.BombPoints)
+                {
+                    spawnComponent.Spawn();
+                }
+                yield return new WaitForSeconds(bombSequence.Delay);
+            }
+
+            _coroutine = null;
+        }
+
+
+        [Serializable]
+        public class BombSequence
+        {
+            [SerializeField] private SpawnComponent[] _bombPoints;
+            [SerializeField] private float _delay;
+
+            public SpawnComponent[] BombPoints => _bombPoints;
+            public float Delay => _delay;
+        }
+    }
+}
---
+++ Assets/Scripts/Creatures/Boss/BossBombingStageState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Creatures.Boss.Bombs;
+using UnityEngine;
+
+public class BossBombingStageState : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var bombController = animator.GetComponent<BombsController>();
+        bombController.StartBombing();
+    }
+}
